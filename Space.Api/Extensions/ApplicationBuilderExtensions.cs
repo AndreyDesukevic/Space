93262b955676c88dc82087ca52f719da926e87aa
AndreyDesukevic <andrejdesukevic@gmail.com>
2025-12-01 10:43:24 +0300

    рефакторинг progpam

---
+++ Space.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+using Space.Application.Interfaces;
+
+namespace Space.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder AddRecurringJobs(this IApplicationBuilder app)
+    {
+        RecurringJob.AddOrUpdate<IMeteoriteSyncJob>(
+            "MeteoriteSyncJob_Daily",
+            job => job.RunAsync(),
+            Cron.Daily);
+
+        return app;
+    }
+}
--- Space.Api/Program.cs
+++ Space.Api/Program.cs
@@ -1,117 +1,25 @@
 using Hangfire;
-using Hangfire.PostgreSql;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi;
-using Polly;
-using Polly.Extensions.Http;
-using Serilog;
 using Space.Api.Auth;
-using Space.Application.Interfaces;
-using Space.Application.Jobs;
-using Space.Application.Mapping;
-using Space.Application.Services;
-using Space.Infrastructure.Database;
-using Space.Infrastructure.Extensions;
-using Space.Infrastructure.HttpClients;
-using Space.Infrastructure.Options;
+using Space.Api.Extensions;
 using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5000);
-});
+builder.WebHost.ConfigureKestrel(o => o.ListenAnyIP(5000));
 
-
-builder.Services.AddDbContext<SpaceDbContext>(options =>
-{
-    options.UseNpgsql(connectionString);
-    options.UseLazyLoadingProxies();
-});
-
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-});
-
-builder.Services.AddHangfire((serviceProvider, configuration) =>
-{
-    configuration.UsePostgreSqlStorage(
-        options =>
-        {
-            options.UseNpgsqlConnection(connectionString);
-        },
-        new PostgreSqlStorageOptions
-        {
-            SchemaName = "hangfire",
-            PrepareSchemaIfNecessary = true,
-            QueuePollInterval = TimeSpan.FromSeconds(5)
-        });
-});
-
-builder.Services.AddHttpClient("MeteoriteClient", client =>
-{
-    client.Timeout = TimeSpan.FromSeconds(30);
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-})
-.AddPolicyHandler(GetRetryPolicy())
-.AddPolicyHandler(GetCircuitBreakerPolicy());
-
-builder.Services.AddScoped<IMeteoriteSyncClient, MeteoriteSyncClient>();
-builder.Services.AddScoped<IMeteoriteService, MeteoriteService>();
-builder.Services.AddScoped<IMeteoriteSyncJob, MeteoriteSyncJob>();
-builder.Services.AddScoped<IRecClassService, RecClassService>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy
-            .WithOrigins("http://localhost:5173", "http://104.248.32.16")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-    });
-});
-
-static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-{
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .OrResult(msg => (int)msg.StatusCode == 429)
-        .WaitAndRetryAsync(
-            3,
-            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
-        );
-}
-
-static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
-{
-    return HttpPolicyExtensions
-        .HandleTransientHttpError()
-        .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
-}
-
-builder.Services.AddAutoMapper(cfg => { }, typeof(MeteoriteProfile));
-builder.Services.AddRepositories();
-builder.Services.AddHangfireServer();
+builder.Services.AddAppDb(builder.Configuration);
+builder.Services.AddAppSerilog(builder.Configuration);
+builder.Services.AddAppHangfire(builder.Configuration);
+builder.Services.AddAppHttpClients(builder.Configuration);
+builder.Services.AddAppServices();
+builder.Services.AddAppAutomapper();
+builder.Services.AddAppCors();
+builder.Services.AddAppSwagger();
 builder.Services.AddMemoryCache();
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "SpaceSync API", Version = "v1" });
-    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    options.IncludeXmlComments(xmlPath);
-});
-
-builder.Services.Configure<MeteorDataOptions>(builder.Configuration.GetSection("MeteorData"));
-
 
 var app = builder.Build();
 
@@ -151,14 +59,11 @@ app.UseHangfireDashboard("/hangfire", new DashboardOptions
     Authorization = new[] { new AllowAllDashboardAuthorization() }
 });
 
-RecurringJob.AddOrUpdate<IMeteoriteSyncJob>(
-    "MeteoriteSyncJob_Daily",
-    job => job.RunAsync(),
-    Cron.Daily);
-
 app.UseCors("AllowFrontend");
 
 app.MapControllers();
+
+app.AddRecurringJobs();
 
 app.Run();
 
